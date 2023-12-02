0f1c4e09bfca23a89394cba2ff2539e5bf51a6fd
Egor <itpd2010@yandex.ru>
2023-12-02 11:41:21 +0100

    feat: update rhf form

--- src/components/RHForm/RHForm.tsx
+++ src/components/RHForm/RHForm.tsx
@@ -1,9 +1,8 @@
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 // import { useDispatch } from 'react-redux';
 import { countryList } from '../../utils/counryList';
-import { SubmitHandler } from 'react-hook-form';
 
 import './RHForm.scss';
 
@@ -15,11 +14,11 @@ type FormData = {
   confirmPassword: string;
   gender: string;
   acceptTerms?: boolean;
-  // image?: FileList;
+  userImage: FileList | null;
   country: string;
 };
 
-const validationSchema = yup.object({
+const validationSchema = yup.object<FormData>().shape({
   name: yup
     .string()
     .required('Name is required')
@@ -47,9 +46,19 @@ const validationSchema = yup.object({
     .required('Confirm Password is required'),
   gender: yup.string().required('Gender is required'),
   acceptTerms: yup
-    .boolean()
+    .bool()
     .oneOf([true], 'Accept Terms & Conditions is required'),
-  // image: yup.mixed().test('fileSize', 'File size is too large', (value) => value && value[0].size <= 1024000).required('Image is required'),
+  userImage: yup
+    .mixed<FileList>()
+    .nullable()
+    .test('fileSize', 'File size is too large', (value) => {
+      if (!value || value.length === 0 || !value[0]) {
+        return true;
+      }
+      const file = value[0];
+      return file.size <= 1024000;
+    })
+    .required('Image is required'),
   country: yup.string().required('Country is required'),
 });
 
@@ -61,7 +70,18 @@ const RHForm = () => {
     formState: { errors },
     control,
   } = useForm({
-    resolver: yupResolver(validationSchema),
+    resolver: yupResolver<FormData>(validationSchema),
+    defaultValues: {
+      acceptTerms: false,
+      name: '',
+      age: 0,
+      email: '',
+      password: '',
+      confirmPassword: '',
+      gender: '',
+      userImage: null,
+      country: '',
+    },
   });
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
@@ -73,19 +93,41 @@ const RHForm = () => {
   return (
     <form className="form" onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="name">Name</label>
-      <input {...register('name')} id="name" autoComplete="on" />
+      <input
+        {...register('name')}
+        id="name"
+        placeholder="Enter your name"
+        autoComplete="on"
+      />
       {errors.name && <p>{errors.name.message}</p>}
 
       <label htmlFor="age">Age</label>
-      <input {...register('age')} id="age" type="number" autoComplete="on" />
+      <input
+        {...register('age')}
+        id="age"
+        type="number"
+        placeholder="Enter your age"
+        autoComplete="on"
+      />
       {errors.age && <p>{errors.age.message}</p>}
 
       <label htmlFor="email">Email</label>
-      <input {...register('email')} id="email" type="email" autoComplete="on" />
+      <input
+        {...register('email')}
+        id="email"
+        type="email"
+        placeholder="example@email.com"
+        autoComplete="on"
+      />
       {errors.email && <p>{errors.email.message}</p>}
 
       <label htmlFor="password">Password</label>
-      <input {...register('password')} id="password" type="password" autoComplete="on" />
+      <input
+        {...register('password')}
+        id="password"
+        type="password"
+        autoComplete="on"
+      />
       {errors.password && <p>{errors.password.message}</p>}
 
       <label htmlFor="confirmPassword">Confirm Password</label>
@@ -111,9 +153,14 @@ const RHForm = () => {
         {errors.acceptTerms && <p>{errors.acceptTerms.message}</p>}
       </div>
 
-      {/* <label htmlFor="image">Image</label>
-      <input {...register('image')} type="file" />
-      {errors.image && <p>{errors.image.message}</p>} */}
+      <label htmlFor="image">Upload Image</label>
+      <input
+        {...register('userImage')}
+        type="file"
+        id="image"
+        accept=".jpg, .jpeg, .png"
+      />
+      {errors.userImage && <p>{errors.userImage.message}</p>}
 
       <label htmlFor="country">Country</label>
       <Controller
